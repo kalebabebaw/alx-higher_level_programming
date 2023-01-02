33040ece195f1f947ecf8775feeefc36ce1f4483
kalebabebaw <emailtokaleb@gmail.com>
2023-01-02 11:25:45 -0800

    101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""Solves the N-queens puzzle"""
 
+"""Solves the N-queens puzzle.
+Determines all possible solutions to placing N
+N non-attacking queens on an NxN chessboard.
+"""
 import sys
 
 
